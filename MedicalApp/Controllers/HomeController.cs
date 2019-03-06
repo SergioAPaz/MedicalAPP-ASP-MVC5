fb3037e7e6d7b436db90bcd290c2d176bee9427b
Sergio Paz <alejandroax@live.com.mx>
2019-03-05 20:47:07 -0800

    Mostrar tabla en Home y eliminar registros
    Se implementa carousel en indes con tabla de tareas y la opcion de eliminar resgistro en tabla.

--- MedicalApp/Controllers/HomeController.cs
+++ MedicalApp/Controllers/HomeController.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using MedicalApp.Models;
+using MedicalApp.Models.dbOwnModels;
+using MedicalApp.Models.OwnModels;
+
 
 namespace MedicalApp.Controllers
 {
+
     public class HomeController : Controller
     {
+        private MedicalAppEntities1 db = new MedicalAppEntities1();
+
         public ActionResult Index()
         {
-            return View();
+            var cT_Users = db.CT_Users.Include(c => c.CT_Roles);
+            return View(cT_Users.ToList());
         }
 
         public ActionResult About()
@@ -26,5 +37,22 @@ namespace MedicalApp.Controllers
 
             return View();
         }
+
+       
+        public ActionResult Delete(int? id)
+        {
+            if (id != null)
+            {
+                CT_Users cT_Users = db.CT_Users.Find(id);
+                db.CT_Users.Remove(cT_Users);
+                db.SaveChanges();
+                
+            }
+            return RedirectToAction("Index");
+
+        }
+
+
+
     }
 }
